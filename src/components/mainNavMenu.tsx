b13e73a1557993b6991d29267e9a27aa4d46bef8
ward438 <ward438@gmail.com>
2026-07-11 17:34:05 -0600

    updating button style

--- src/components/mainNavMenu.tsx
+++ src/components/mainNavMenu.tsx
@@ -20,18 +20,20 @@ export const MainNavMenu = () => {
     return (
         <div className="flex w-full h-full">
             <div className={`flex flex-col h-screen border-r-2 border-gray-200 pt-4 gap-2 px-4 transition-all ${isOpen ? 'w-56' : 'w-14'} bg-purple-100`}>
-                <button
-                    onClick={() => setIsOpen(!isOpen)}
-                    className="self-start px-2 py-1 text-sm border rounded cursor-pointer mb-4 bg-yellow-100 text-white"
-                    title={isOpen ? 'Close menu' : 'Open menu'}
-                >
-                    {isOpen ? '✕ Close' : '☰'}
-                </button>
+                <div className={`flex ${isOpen ? 'justify-start' : 'justify-center'}`}>   
+                    <button
+                        onClick={() => setIsOpen(!isOpen)}
+                        className="self-start px-2 py-1 text-sm border rounded cursor-pointer mb-4 bg-warning-primary text-white hover:bg-yellow-100"
+                        title={isOpen ? 'Close menu' : 'Open menu'}
+                    >
+                        {isOpen ? '✕ Close' : '☰'}
+                    </button>
+                </div>
                 {isOpen && navItems.map(({ label }) => (
                     <button
                         key={label}
                         onClick={() => setActiveLabel(label)}
-                        className={`text-left px-3 py-2 rounded cursor-pointer text-creamWhite-100 hover:bg-yellow-100 transition delay-10 duration-300 ease-in-out
+                        className={`text-left px-3 py-2 rounded cursor-pointer text-creamWhite-100 hover:bg-yellow-100
                              hover:scale-110`}>
                         {label}
                     </button>
@@ -39,13 +41,13 @@ export const MainNavMenu = () => {
             </div>            
             <div className="flex-1 min-w-0">
                 <ActiveComponent />
-                <div className="fixed bottom-0 left-0">
+                <div className={`fixed bottom-0 left-0 flex justify-center pb-4 ${isOpen ? 'w-55' : 'w-12.75'} `}>
                     <button 
-                        className={`self-start px-2 py-1 text-sm border rounded cursor-pointer mb-4 bg-yellow-100 text-white`}
+                        className="px-2 py-.5 text-sm border rounded cursor-pointer bg-warning-primary text-creamWhite-100 hover:bg-yellow-100"
+                        title={"Log Out"}
                         onClick={() => setActiveLabel('Log Out')}>
                             <LogOut isOpen={isOpen} />
                     </button>
-                    
                 </div>
         </div>
     </div>
--- src/dashboards/logOut.tsx
+++ src/dashboards/logOut.tsx
@@ -1,8 +1,8 @@
 export const LogOut = ({ isOpen }: { isOpen: boolean }) => {
     return (
         <div className="w-full h-full">
-            <h1 className="text-2xl font-bold text-center">
-            {isOpen ? 'Log Out' : 'X'}
+            <h1 className="text-1xl text-center">
+                {isOpen ? 'Log Out' : 'X'}
             </h1>
         </div>
     );
