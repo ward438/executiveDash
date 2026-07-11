32808b9d0b74add69601f6905780fb62e3be6bd9
ward438 <ward438@gmail.com>
2026-07-11 13:31:18 -0600

    centered header

--- src/dashboards/trendDash.tsx
+++ src/dashboards/trendDash.tsx
@@ -3,6 +3,13 @@ import { TrendChart } from "../charts/trendCharts/trendChart";
 export const TrendDash = () => {
     return (
         <div className="w-full h-full p-8">
+            <div className="flex items-center justify-center">
+                <h1 className="text-2xl font-bold">
+                    <span className="text-center">
+                        Trend Dashboard
+                    </span>
+                </h1>
+            </div>
             <TrendChart />
         </div>
     );
