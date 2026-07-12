9672da76f9d57ecc3f073c12ea2972fc598bc4f9
ward438 <ward438@gmail.com>
2026-07-11 20:21:58 -0600

    removed unused line

--- src/charts/trendCharts/trendChart.tsx
+++ src/charts/trendCharts/trendChart.tsx
@@ -1,12 +1,11 @@
 import { Chart } from "react-chartjs-2";
 import { trendChartConfig } from "./config/trendChartConfig";
-import costsData from "../../data/costs.json";
 import { dateFilteredJson } from "./chartData/trendChartData";
 
 export const TrendDropDown = ({ isOpen, setIsOpen }: { isOpen: boolean; setIsOpen: (isOpen: boolean) => void }) => {        
     return <>  
         <button onClick={() => setIsOpen(!isOpen)} className=" text-warning-primary inline-flex items-center justify-center border font-medium rounded-base text-sm px-4 py-2.5 cursor-pointer" >
-            Dropdown button 
+            Over Budget Items
             {/* This is my carrot, need to import svg */}
             <svg className="w-4 h-4 ms-1.5 -me-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 9-7 7-7-7"/></svg>
         </button>
