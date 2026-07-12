a5014be3b94455c736830026bd5628d07325d711
ward438 <ward438@gmail.com>
2026-07-11 21:49:24 -0600

    added default value to drop down, sets selected account to null

--- src/charts/trendCharts/components/trendDropDown.tsx
+++ src/charts/trendCharts/components/trendDropDown.tsx
@@ -4,7 +4,7 @@ export const TrendDropDown = (
     { isOpen, setIsOpen, setSelectedAccount }: 
     { isOpen: boolean; setIsOpen: (isOpen: boolean) => void, selectedAccount: { account_name: string; cost: number; budget: number } | 
     null, setSelectedAccount: (selectedAccount: { account_name: string; cost: number; budget: number } | null) => void }
-) => {      
+) => {     
     return <>  
         <button onClick={() => setIsOpen(!isOpen)} className=" text-warning-primary inline-flex items-center justify-center border font-medium rounded-base text-sm px-4 py-2.5 cursor-pointer" >
             Over Budget Items
@@ -14,6 +14,9 @@ export const TrendDropDown = (
                 
         {isOpen && (
             <div className="absolute bg-white border z-10 flex flex-col w-80 max-h-64 overflow-y-auto shadow-md">
+                <button onClick={() => setSelectedAccount(null)} className="text-left px-3 py-1.5 text-sm hover:bg-yellow-100 border-b font-semibold">
+                    All Accounts
+                </button>
                 {dateFilteredJson.filter((data) => data != null).map((data: any, index: number) => (
                     <button onClick={() => setSelectedAccount({account_name: data.account_name, cost: data.cost, budget: data.budget})} key={index} className="text-left px-3 py-1.5 text-sm hover:bg-yellow-100 border-b last:border-b-0">
                         {`${data.account_name} | cost:  $${data.cost} | budget: $${data.budget}`}
