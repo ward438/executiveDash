b14cbc0f830fbb6063ea3d2da62d69c14f1b041b
ward438 <ward438@gmail.com>
2026-07-12 11:15:34 -0600

    imported overBudgetItems to be used in table for CostBreakdowns

--- src/dashboards/costBreakdowns.tsx
+++ src/dashboards/costBreakdowns.tsx
@@ -1,4 +1,27 @@
+import { overBudgetItems } from "../charts/trendCharts/chartData/trendChartData";
+
 export const CostBreakdowns = () => {
+    console.log('overBudgetItems: ', overBudgetItems);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return (
         <div className="w-full h-full p-8">
             <h1 className="text-2xl font-bold text-center">Cost Breakdowns</h1>
