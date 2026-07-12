b70251280fb2385212682c60a30539a72ccbf86e
ward438 <ward438@gmail.com>
2026-07-12 14:23:00 -0600

    styling interpolated containers

--- src/components/tables/costBreakdownTable.tsx
+++ src/components/tables/costBreakdownTable.tsx
@@ -87,11 +87,17 @@ export const CostBreakdownsTable = ({ currentItems, page, totalPages, handlePage
                         <button className="cursor-pointer px-3 py-1 border rounded disabled:opacity-40 float-right bg-warning-primary text-creamWhite-100" onClick={() => setIsModalOpen(false)}>Close</button>
                         <div className="flex flex-col items-center justify-center">
                             <h2 className="text-1xl text-white text-center">{selectedRow.account_name}</h2>
-                            <p className="text-sm text-white">{selectedRow.owner}</p>
-                            <p className="text-sm text-white">{selectedRow.service}</p>
-                            <p className="text-sm text-white">{selectedRow.region}</p>
-                            <p className="text-sm text-white">{selectedRow.cost}</p>
-                            <p className="text-sm text-white">{selectedRow.budget}</p>
+                        </div>
+                        <div className="grid grid-flow-col grid-rows-3 gap-4">
+                        <div className="row-span-3 ... space-y-5">
+                            <p className="text-sm text-white"><span>Owner: </span><span className="border border-gray-300 p-1 rounded">{ selectedRow.owner}</span></p>
+                            <p className="text-sm text-white"><span>Service: </span><span className="border border-gray-300 p-1 rounded">{selectedRow.service}</span></p>
+                            <p className="text-sm text-white"><span>Region: </span><span className="border border-gray-300 p-1 rounded">{selectedRow.region}</span></p>
+                            <p className="text-sm text-white"><span>Cost: </span><span className="border border-gray-300 p-1 rounded"   >{selectedRow.cost}</span></p>
+                            <p className="text-sm text-white"><span>Budget: </span><span className="border border-gray-300 p-1 rounded">{selectedRow.budget}</span></p>
+                        </div>
+                        <div className="col-span-2 ...">02</div>
+                        <div className="col-span-2 row-span-2 ...">03</div>
                         </div>
                     </div>
                 </div>
